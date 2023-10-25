2596c23224f275acc49ad60427ec483faf6e0c9c
Rishj15 <148879674+Rishj15@users.noreply.github.com>
2023-10-25 14:06:41 -0400

    Update SigninPageStepDefinitions.java

--- src/test/java/StepDefinitions/SigninPageStepDefinitions.java
+++ src/test/java/StepDefinitions/SigninPageStepDefinitions.java
@@ -1,6 +1,8 @@
 package StepDefinitions;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import Pages.Signin;
 import io.cucumber.java.en.Given;
@@ -8,13 +10,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import managers.PageObjectManager;
 import managers.WebDriverManager;
+import cucumber.TestContext;
 
 public class SigninPageStepDefinitions {
-	
-	WebDriver driver;
+	TestContext testContext;
+	//WebDriver driver;
 	Signin signinPage;
-	WebDriverManager webDriverManager;
-	PageObjectManager pageObjectManager;
+	//WebDriverManager webDriverManager;
+	//PageObjectManager pageObjectManager;
+	
+	public SigninPageStepDefinitions(TestContext context) {
+		testContext = context;
+		signinPage = testContext.getPageObjectManager().getSigninPage();
+	}
+	
 	
 	
 	
@@ -22,13 +31,20 @@ public class SigninPageStepDefinitions {
 	@Given("User is on DSAlgo Portal sign in page")
 	public void user_is_on_ds_algo_portal_sign_in_page() {
 		
-		webDriverManager = new WebDriverManager();
-		driver = webDriverManager.getDriver();
-		pageObjectManager = new PageObjectManager(driver);
-		signinPage = pageObjectManager.getSigninPage();
+		//webDriverManager = new WebDriverManager();
+		//driver = webDriverManager.getDriver();
+		//pageObjectManager = new PageObjectManager(driver);
+		//signinPage = pageObjectManager.getSigninPage();
 		signinPage.navigateTo_Signinpage();
 	}
-
+	
+	
+	@When ("user clicks sign in")
+	public void user_clicks_sign_in() {
+		signinPage.clickSignin();
+	}
+	
+	
 	@When("User enter valid {string} and {string}")
 	public void user_enter_valid_and(String username, String password1) {
 		signinPage.entercredentials(username, password1);
